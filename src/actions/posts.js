5849fb8be842d7e9b124978a6a9a4c454cab48a1
Ankit Aman <ankit7091aman@gmail.com>
2022-03-25 18:08:38 +0530

    First commit

---
+++ src/actions/posts.js
@@ -0,0 +1,16 @@
+import * as api from '../api';
+
+// Action creators 
+
+const getPosts = () => async(dispatch){
+    try
+
+
+
+
+
+
+    const action = { type: 'FETCH_ALL', payload: []}
+
+   dispatch(action);
+}
